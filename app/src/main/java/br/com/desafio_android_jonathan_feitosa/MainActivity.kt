ae47e66088bd2f1f92fc6f83743f683dde3a24f0
Jonathan Feitosa <jonathan.souza@itriad.org.br>
2020-05-28 04:29:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/desafio_android_jonathan_feitosa/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.desafio_android_jonathan_feitosa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+    }
+}
